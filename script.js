6bb1e531661578546901bb588ae345d4618894d4
ryclos <loogle@hotmail.com>
2017-01-05 02:36:39 -1000

    Changement

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+var x = 12;
+var y = 75.23;
+var name = "Freddy";
+var xString = '12';
+var myString = '12.63';
+var myStringName = 'Hello Hiomai';
+var string1 = 'Hello';
+var string2 = 'Hiomai';
+
+
+var stringAssemblage = string1 + " " + string2;
+var myNewString = myStringName.replace("Hiomai", "Tim");
+var position = myStringName.indexOf("Hiomai");
+var xFloat = parseFloat(myString);
+var xNumber = parseInt(xString);
+var xToString = x.toString();
+var nameLength = name.length;
+
+console.log(x);
+console.log(y);
+console.log(nameLength);
+console.log(xNumber);
+console.log(xFloat);
+console.log(position);
+console.log(myNewString);
+console.log(stringAssemblage);
+*/
+
+var x = 25;
+var y = 12;
+var result = x % y;
+
+console.log(result);
